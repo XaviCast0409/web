b58d6aacb1a1aa984c96a6597a6ed36d58a08926
XaviCast0409 <xaviermontero.0409.1995@gmail.com>
2024-11-22 22:06:32 -0500

    front

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+import axios from "axios";
+
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    axios
+      .post("http://localhost:3000/send-email", formData)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <section id="contact" className="bg-gray-200 py-10">
       <div className="container mx-auto px-4">
@@ -11,6 +38,9 @@ const ContactForm = () => {
             <input
               type="text"
               className="w-full border-gray-300 rounded-lg p-2"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -18,6 +48,9 @@ const ContactForm = () => {
             <input
               type="email"
               className="w-full border-gray-300 rounded-lg p-2"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -25,18 +58,25 @@ const ContactForm = () => {
             <input
               type="text"
               className="w-full border-gray-300 rounded-lg p-2"
+              value={formData.phone}
+              name="phone"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700">Message</label>
             <textarea
               className="w-full border-gray-300 rounded-lg p-2"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
               rows="4"
             ></textarea>
           </div>
           <button
             type="submit"
             className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
+            onClick={onSubmit}
           >
             Send Message
           </button>
